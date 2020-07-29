36ffba501618e524a70b4afc363b98407c3b5923
jaxyt <jax.maximillion.taylor@gmail.com>
2020-07-29 20:10:51 +0000

    added acronyms

--- states/models.py
+++ states/models.py
@@ -6,6 +6,7 @@ from django import forms
 class State(models.Model):
 
     statename = models.CharField(max_length=200)
+    stateacronym = models.CharField(max_length=5, blank=True)
     statengram = models.TextField(blank=True)
 
     objects = models.DjongoManager()
