4039baed16cf001855da1cca668fb93987cd4bbd
jaxyt <jax.maximillion.taylor@gmail.com>
2020-07-29 15:48:07 -0400

    debug

--- compiler/mongo_functions.py
+++ compiler/mongo_functions.py
@@ -332,12 +332,13 @@ def render_xml_sitemap(s, t, rt):
                     url_cnt += 1
             else:
                 for n in coll_ra.find().distinct("company"):
-                    #  sitemap_urls.append(f"""<url><loc>https://www.{s.sitename}.com/registered-agents/search/company/{n['company']}</loc></url>""")
+                    sitemap_urls.append(f"""<url><loc>https://www.{s.sitename}.com/registered-agents/search/company/{n['company']}</loc></url>""")
                     url_cnt += 1
         elif re.search(r'^/process-server/', i.route) is not None:
             if re.search(r'^/process-server/id/state', i.route) is not None:
-                print(len(coll_cp.find()))
-                print(len(coll_ra.find().distinct("state")))
+                for n in coll_cp.find():
+                    sitemap_urls.append(f"""<url><loc>https://www.{s.sitename}.com/process-server/{"-".join(n['name'].split(" ")).lower()}-{n['id']}</loc></url>""")
+                    url_cnt += 1
         else:
             if re.search(r'\.[a-z]{2,4}$', i.route) is None:
                 sitemap_urls.append(f"""<url><loc>https://www.{s.sitename}.com{i.route}</loc></url>""")
