f54946a30fc015f7446705c43ce75414e7486f5b
jaxyt <jax.maximillion.taylor@gmail.com>
2020-07-31 10:21:35 -0400

    blah

--- compiler/views.py
+++ compiler/views.py
@@ -1008,9 +1008,13 @@ def get_registered_agents(request):
 
 def pull_from_github(request, *args, **kwargs):
     if request.method == 'POST':
+        print(request)
+        print(request.POST)
         if request.POST.get("payload"):
             os.popen("cd ~/ace/aceapiversion2/ && git reset –hard HEAD && git pull").read()
             return HttpResponse(status=204)
+        else:
+            return HttpResponse("unsuccessful")
     else:
         return HttpResponse("hello world")
-        
+
