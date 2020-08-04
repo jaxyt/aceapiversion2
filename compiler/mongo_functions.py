d12723628f3e00cc8aebbadc806c4ba34ab1c1fa
jaxyt <jax.maximillion.taylor@gmail.com>
2020-08-04 16:09:44 -0400

    b

--- compiler/mongo_functions.py
+++ compiler/mongo_functions.py
@@ -473,24 +473,21 @@ def sort_results(results, quer):
             avg_cnt += 1
         if val['city']:
             avg_cnt += 1
-        scores.append({"obj": val, "averagescore": (similar_text(val['company'], quer)+similar_text(val['agency'], quer)+similar_text(val['state'], quer)+similar_text(val['city'], quer))/avg_cnt, "scorearr": [similar_text(val['company'], quer), similar_text(val['agency'], quer), similar_text(val['state'], quer), similar_text(val['city'], quer)]})
+        # scores.append({"obj": val, "averagescore": (similar_text(val['company'], quer)+similar_text(val['agency'], quer)+similar_text(val['state'], quer)+similar_text(val['city'], quer))/avg_cnt, "scorearr": [similar_text(val['company'], quer), similar_text(val['agency'], quer), similar_text(val['state'], quer), similar_text(val['city'], quer)]})
+        scores.append({"obj": val, "averagescore": similar_text(f"{val['company']} {val['agency']} {val['city']} {val['state']}", q), "scorearr": [similar_text(val['company'], quer), similar_text(val['agency'], quer), similar_text(val['state'], quer), similar_text(val['city'], quer)]})
     for val in scores:
         if len(sorted_scores) == 0:
             sorted_scores.append(val)
         else:
             for idx, n in enumerate(sorted_scores):
-                # if val['averagescore'] > n['averagescore']:
-                if max(val['scorearr']) > max(n['scorearr']):
+                if val['averagescore'] > n['averagescore']:
                     sorted_scores.insert(idx, val)
                     break
-                # elif idx == len(sorted_scores) - 1 and val['averagescore'] < n['averagescore']:
-                elif idx == len(sorted_scores) - 1 and max(val['scorearr']) < max(n['scorearr']):
+                elif idx == len(sorted_scores) - 1 and val['averagescore'] < n['averagescore']:
                     sorted_scores.append(val)
                     break
-                # elif val['averagescore'] == n['averagescore']:
-                elif max(val['scorearr']) == max(n['scorearr']):
-                    # if max(val['scorearr']) > max(n['scorearr']):
-                    if val['averagescore'] > n['averagescore']:
+                elif val['averagescore'] == n['averagescore']:
+                    if max(val['scorearr']) > max(n['scorearr']):
                         sorted_scores.insert(idx, val)
                         break
     return sorted_scores
