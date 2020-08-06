e2955a9b70f0f4905bbf5586412af146715b035e
jaxyt <jax.maximillion.taylor@gmail.com>
2020-08-06 13:10:45 -0400

    b

--- compiler/mongo_functions.py
+++ compiler/mongo_functions.py
@@ -292,7 +292,7 @@ def compiler_v3(s, t, r, arr):
             comp = re.sub('XXcorpXX', corp['name'], comp)
             comp = re.sub('XXstatequeryXX', st.title(), comp)
             corps_in_cities = """<div class="state-corps-links">"""
-            for i in coll_ra.find({k: query, 'state': state_query}).distinct('city'):
+            for i in coll_ra.find({k: query, 'state': state_query}):
                 corps_in_cities += f"""<a href="{"/".join(arr)}/{"-".join(i['city'].split(" "))}">{i['city'].title()}</a>""" if i['city'] else ""
             corps_in_cities += """</div>"""
             comp = re.sub('XXcitycorpsXX', corps_in_cities, comp)
