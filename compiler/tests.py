e0aea4010ed633e13c2f29845354a90be1b4606a
jaxyt <jax.maximillion.taylor@gmail.com>
2020-08-06 12:25:05 -0400

    b

--- compiler/tests.py
+++ compiler/tests.py
@@ -192,10 +192,7 @@ def example():
         data = json.load(json_file)
         for n in trange(len(cities), bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.RED, Fore.RESET)):
             i = cities[n]['_id']
-            found = False
             for idx, val in enumerate(data):
-                if found:
-                    break
                 for attr, value in val['properties'].items():
                     if type(value) == type(i['cityname']) and attr != "gnis:County" and attr != "is_in":
                         reg = re.compile(i['cityname'], re.IGNORECASE)
