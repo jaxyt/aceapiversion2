29741ad5e15780f2d82c38d087f16872284f351e
jaxyt <jax.maximillion.taylor@gmail.com>
2020-08-07 11:55:13 -0400

    b

---
+++ compiler/tests2.py
@@ -0,0 +1,37 @@
+import pymongo
+import re
+import json
+import datetime
+import os
+import pprint as pp
+from pymongo import MongoClient
+from similar_text import similar_text
+import Levenshtein
+import string
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+stw = stopwords.words('english')
+import ngram
+import timeit
+from tqdm import tqdm, trange
+from colorama import Fore
+
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.acedbv2
+
+coll_ra = db.registeredagents_registeredagent
+coll_st = db.states_state
+coll_co = db.counties_county
+coll_ci = db.cities_city
+coll_si = db.sites_site
+coll_te = db.templates_template
+coll_bl = db.blogs_blog
+coll_cp = db.registeredagents_corporation
+coll_te = db.registeredagents_telecomcorps
+
+
+def lev_and_cos_search():
+    return
+    
