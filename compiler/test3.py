b4efae5c99d7071f90669ecde2580e5d65fd7938
jaxyt <jax.maximillion.taylor@gmail.com>
2020-08-20 11:53:59 -0400

    b

---
+++ compiler/test3.py
@@ -0,0 +1,52 @@
+import pymongo
+import re
+import json
+import datetime
+import os
+import pprint as pp
+from pymongo import MongoClient
+from similar_text import similar_text
+import Levenshtein
+import string
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+stopwords = stopwords.words('english')
+import ngram
+import timeit
+from tqdm import tqdm, trange
+from colorama import Fore
+
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.acedbv2
+
+coll_ra = db.registeredagents_registeredagent
+coll_st = db.states_state
+coll_co = db.counties_county
+coll_ci = db.cities_city
+coll_si = db.sites_site
+coll_te = db.templates_template
+coll_bl = db.blogs_blog
+coll_cp = db.registeredagents_corporation
+coll_te = db.registeredagents_telecomcorps
+
+
+def add_to_map(ob):
+    return ob
+
+def clean_string(text):
+    text = ''.join([word for word in text if word not in string.punctuation])
+    text = text.lower()
+    text = ' '.join([word for word in text.split() if word not in stopwords])
+
+    return text
+
+def cosine_sim_vectors(vec1, vec2):
+    vec1 = vec1.reshape(1, -1)
+    vec2 = vec2.reshape(1, -1)
+    return cosine_similarity(vec1, vec2)[0][0]
+
+
+def telecom_search():
+    return
