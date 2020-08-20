5774d3ee9194c427da207ba9129437f1d302d011
jaxyt <jax.maximillion.taylor@gmail.com>
2020-08-20 17:15:03 -0400

    b

--- compiler/test3.py
+++ compiler/test3.py
@@ -16,6 +16,8 @@ import ngram
 import timeit
 from tqdm import tqdm, trange
 from colorama import Fore
+from fuzzywuzzy import process
+
 
 
 client = MongoClient('mongodb://localhost:27017/')
@@ -35,6 +37,9 @@ coll_te = db.registeredagents_telecomcorps
 def add_to_map(ob):
     return ob
 
+def add_to_map_tuple(ob):
+    return ob[1]
+
 def clean_string(text):
     text = ''.join([word for word in text if word not in string.punctuation])
     text = text.lower()
@@ -51,25 +56,23 @@ def cosine_sim_vectors(vec1, vec2):
 def telecom_search(searchterm, model_keys):
     results = {}
     agents = list(map(add_to_map, coll_te.find()))
-
     for n in trange(len(agents), bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.RED, Fore.RESET)):
         i = agents[n]
-        sentences = [searchterm]
-        combined = ""
+        str2Match = searchterm
+        strOptions = []
         for k in model_keys:
             if i[k]:
-                sentences.append(i[k])
-                combined += f"{i[k]} "
-        combined = combined.strip()
-        #sentences.append(combined)
-        cleaned = list(map(clean_string, sentences))
-        vectorizer = CountVectorizer().fit_transform(cleaned)
-        vectors = vectorizer.toarray()
-        similarities = [cosine_sim_vectors(vectors[0], k) for k in vectors[1:]]
-        avg_similarity = sum(similarities)/(len(similarities) + 1)
-        results[f"{i['id']}"] = avg_similarity
+                strOptions.append(i[k])
+        Ratios = process.extract(str2Match,strOptions)
+        avg = sum(list(map(add_to_map_tuple, Ratios)))/len(Ratios)
+
+        highest = process.extractOne(str2Match,strOptions)
+        #results.append({"id": i['id'], "highest": highest, "average": avg})
+        results[f"{i['id']}"] = max([highest, avg])
     return sorted(results.items(), key=lambda x: x[1], reverse=True)
 
+        
 
-results = telecom_search("ver", ['carriername', 'businessname', 'holdingcompany', 'othertradename1', 'othertradename2', 'othertradename3', 'othertradename4', 'dcagent1', 'dcagent2', 'dcagentcity', 'dcagentstate', 'alternateagent1', 'alternateagent2', 'alternateagentcity', 'alternateagentstate'])
-pp.pprint(results[0:25])
+#  , ['carriername', 'businessname', 'holdingcompany', 'othertradename1', 'othertradename2', 'othertradename3', 'othertradename4', 'dcagent1', 'dcagent2', 'dcagentcity', 'dcagentstate', 'alternateagent1', 'alternateagent2', 'alternateagentcity', 'alternateagentstate']
+r = telecom_search("ver", ['carriername', 'businessname', 'holdingcompany', 'othertradename1', 'othertradename2', 'othertradename3', 'othertradename4', 'dcagent1', 'dcagent2', 'dcagentcity', 'dcagentstate', 'alternateagent1', 'alternateagent2', 'alternateagentcity', 'alternateagentstate'])
+pp.pprint(r[0:25])
