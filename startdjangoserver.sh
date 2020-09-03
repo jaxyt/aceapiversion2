a41f63fc2dfe171f210a3e59bf26842553644bf0
jaxyt <jax.maximillion.taylor@gmail.com>
2020-09-03 10:07:07 -0400

    reduced workers

--- startdjangoserver.sh
+++ startdjangoserver.sh
@@ -1,2 +1,2 @@
-nohup gunicorn --bind 127.0.0.1:8000 aceapiversion2.wsgi:application --reload --access-logfile errlogs.log --workers=3 > my.log 2>&1 &
+nohup gunicorn --bind 127.0.0.1:8000 aceapiversion2.wsgi:application --reload --access-logfile errlogs.log --workers=2 > my.log 2>&1 &
 echo $! > save_pid.txt
