3a18f41a69f5d077a18003b0e37541277551a28d
jaxyt <jax.maximillion.taylor@gmail.com>
2020-09-14 14:42:31 -0400

    b

--- startdjangoserver.sh
+++ startdjangoserver.sh
@@ -1,2 +1,2 @@
-nohup gunicorn --bind 127.0.0.1:8000 aceapiversion2.wsgi:application --reload --access-logfile errlogs.log > my.log 2>&1 &
+nohup gunicorn --bind 127.0.0.1:8000 aceapiversion2.wsgi:application --reload --workers=2 --access-logfile errlogs.log > my.log 2>&1 &
 echo $! > save_pid.txt
