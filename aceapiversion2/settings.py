1fe355d960074ee7d93f89165eae4dd19a69c7c2
jaxyt <jax.maximillion.taylor@gmail.com>
2020-09-15 12:25:54 -0400

    profiling

--- aceapiversion2/settings.py
+++ aceapiversion2/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'aceapiversion2.middleware.ProfilerMiddleware',
 ]
 
 ROOT_URLCONF = 'aceapiversion2.urls'
