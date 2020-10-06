abbd21d5c5c7be9eb80dcaa9a9327e7c57a05345
jaxyt <jax.maximillion.taylor@gmail.com>
2020-10-06 17:34:48 -0400

    b

--- aceapiversion2/urls.py
+++ aceapiversion2/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('compilev4/<int:site_id>/<page_route>/',views.compile_v4, name="compilerv4"),
+    path('compilev4/<int:site_id>/',views.compile_v4, name="compilerv4home"),
+    path('compilev4/<int:site_id>/<page_route>/',views.compile_v4, name="compilerv4routes"),
     path('compilev3/',views.test_send, name="test"),
     path('compile/', views.compile, name='compile'),
     path('compilev2/', views.send_page, name='sendpage'),
