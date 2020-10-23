31fbf332ecd99cf2ae61d2c7b939f651fb8b150d
jax taylor <jax.maximillion.taylor@gmail.com>
2020-10-23 01:53:30 -0400

    b

--- static/codemirror/codemirror-5.57.0/addon/hint/spellcheck-hint.js
+++ static/codemirror/codemirror-5.57.0/addon/hint/spellcheck-hint.js
@@ -58129,56 +58129,14 @@
 
 
     var word = options && options.word || WORD;
-    var range = options && options.range || RANGE;
     var cur = editor.getCursor(), curLine = editor.getLine(cur.line);
     console.log(cur)
     console.log(curLine)
     var end = cur.ch, start = end;
     while (start && word.test(curLine.charAt(start - 1))) --start;
-    var curWord = start != end && curLine.slice(start, end);
-
-    var testList = options && options.list || [];
-
-    var list = options && options.list || [], seen = {};
-    var re = new RegExp(word.source, "g");
-    /*for (var dir = -1; dir <= 1; dir += 2) {
-      var line = cur.line, endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
-      for (; line != endLine; line += dir) {
-        var text = editor.getLine(line), m;
-        while (m = re.exec(text)) {
-          testList.push({
-            'word': word,
-            'range': range,
-            'cur': cur,
-            'curLine': curLine,
-            'end': end,
-            'start': start,
-            'curWord': curWord,
-            'list': list,
-            'seen': seen,
-            're': re,
-            'dir': dir,
-            'line': line,
-            'endlineequation': `Math.min(Math.max(${line} + ${dir} * ${range}, ${editor.firstLine()}), ${editor.lastLine()}) + ${dir} = ${endLine}`,
-            'endLine': endLine,
-            'text': text,
-            'm': m,
-            'loopResult': line == cur.line && m[0] === curWord
-          })
-          if (line == cur.line && m[0] === curWord) continue;
-          if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0])) {
-            seen[m[0]] = true;
-            list.push(m[0]);
-          }
-        }
-      }
-    }*/
-
-    //, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)
-
-    //console.log(testList);
-    re = new RegExp(`${editor.getRange(CodeMirror.Pos(cur.line, start),CodeMirror.Pos(cur.line, end))}[A-z]+`, 'g');
-    list = longText.match(re);
+    
+    var re = new RegExp(`${editor.getRange(CodeMirror.Pos(cur.line, start),CodeMirror.Pos(cur.line, end))}[A-z]+`, 'g');
+    var list = longText.match(re);
     return {list: list, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end), supportsSelection: supportsSelection};
   });
 });
