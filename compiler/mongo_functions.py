8f7510bbdc5cfb6dd41e553e6c2d7a4604e97b41
jaxyt <jax.maximillion.taylor@gmail.com>
2020-11-11 10:16:26 -0500

    b

--- compiler/mongo_functions.py
+++ compiler/mongo_functions.py
@@ -716,7 +716,7 @@ def do_mongo_query(iden, cit):
     res = False
     try:
         #, {"$set": {"city": cit}}
-        x = list(map(add_to_map, coll_ra.find({"city": "New York"})))
+        x = list(map(add_to_map, coll_ra.find({"id": int(iden)})))
         print(x)
         res = True
     except Exception as e:
