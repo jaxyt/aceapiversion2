d4014ef2f18bb411058a69afe082e0facd3b6a7e
jaxyt <jax.maximillion.taylor@gmail.com>
2021-02-10 16:11:30 -0500

    b

--- agents/functions.py
+++ agents/functions.py
@@ -75,23 +75,23 @@ def static_page(request, site, pagedoc, **kwargs):
         
     }
     compiled = f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    XXsitemetasXX
-    XXpagemetasXX
-    XXsitelinksXX
-    XXpagelinksXX
-    XXsitestyleXX
-    <title>XXtitleXX</title>
-</head>
-<body>
-    XXsiteheaderXX
-    XXcontentXX
-    XXsitefooterXX
-    XXsitescriptsXX
-    XXpagescriptsXX
-</body>
-</html>"""
+    <html lang="en">
+    <head>
+        XXsitemetasXX
+        XXpagemetasXX
+        XXsitelinksXX
+        XXpagelinksXX
+        XXsitestyleXX
+        <title>XXtitleXX</title>
+    </head>
+    <body>
+        XXsiteheaderXX
+        XXcontentXX
+        XXsitefooterXX
+        XXsitescriptsXX
+        XXpagescriptsXX
+    </body>
+    </html>"""
 
     for k, v in rep_codes.items():
         compiled = re.sub(k, v, compiled)
@@ -121,32 +121,32 @@ def individual_agent(request, site, pagename, **kwargs):
             
         }
         compiled = f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    XXsitemetasXX
-    XXpagemetasXX
-    XXsitelinksXX
-    XXpagelinksXX
-    XXsitestyleXX
-    <title>XXtitleXX</title>
-</head>
-<body>
-    <!-- XXagentXX -->
-    <!-- XXstateXX -->
-    <!-- XXstateacronymXX -->
-    <!-- XXcountyXX -->
-    <!-- XXcityXX -->
-    <!-- XXzipXX -->
-    <!-- XXaddressXX -->
-    <!-- XXupdatedXX -->
-    <!-- XXcreatedXX -->
-    XXsiteheaderXX
-    XXcontentXX
-    XXsitefooterXX
-    XXsitescriptsXX
-    XXpagescriptsXX
-</body>
-</html>"""
+        <html lang="en">
+        <head>
+            XXsitemetasXX
+            XXpagemetasXX
+            XXsitelinksXX
+            XXpagelinksXX
+            XXsitestyleXX
+            <title>XXtitleXX</title>
+        </head>
+        <body>
+            <!-- XXagentXX -->
+            <!-- XXstateXX -->
+            <!-- XXstateacronymXX -->
+            <!-- XXcountyXX -->
+            <!-- XXcityXX -->
+            <!-- XXzipXX -->
+            <!-- XXaddressXX -->
+            <!-- XXupdatedXX -->
+            <!-- XXcreatedXX -->
+            XXsiteheaderXX
+            XXcontentXX
+            XXsitefooterXX
+            XXsitescriptsXX
+            XXpagescriptsXX
+        </body>
+        </html>"""
 
         for k, v in rep_codes.items():
             compiled = re.sub(k, v, compiled)
@@ -159,7 +159,107 @@ def individual_agent(request, site, pagename, **kwargs):
         return HttpResponseNotFound()
 
 def agents_by_location(request, site, pagename, **kwargs):
-    compiled = "agents by location"
+    dbg = True
+    pagedoc = None
+    route = ""
+    lwargs = dict()
+    if len(kwargs) >= 1:
+        lwargs['state'] = kwargs['arg_one']
+        route = "/agents-by-state/state"
+        if len(kwargs) == 2:
+            lwargs['city'] = kwargs['arg_two']
+            route = "/agents-by-state/state/city"
+
+    for i in site.pages:
+            if i.route == route:
+                pagedoc = i
+                break
+
+    agents_objs = list(coll_ra.find(lwargs, {'_id': 0}))
+
+    agent_table = """
+    <div class="table-responsive">
+        <table id="default_order" class="table table-striped table-bordered display" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Details</th>
+                    <th>Registered Agent</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+    """
+    for i in agents_objs:
+        rel_link = urllib.parse.quote(f"/registered-agents/{i['agent']}/{i['id']}/")
+        if dbg is True:
+            rel_link = urllib.parse.quote(f"/agents/compile/{site.id}/registered-agents/{i['agent']}/{i['id']}/")
+        agent_table += f"""
+                <tr>
+                    <td><a href="{rel_link}"><button type="button" class="btn waves-effect waves-light btn-info">Info</button></a></td>
+                    <td><a href="{rel_link}">{i['agent']}</a></td>
+                    <td><a href="{rel_link}">{i['city']}, {i['state']}</a></td>
+                </tr>
+        """
+    agent_table += """
+            </tbody>
+        </table>
+    </div>
+    """
+    location_table = ""
+    if len(lwargs) < 2:
+        location_table = """<div class="process-server-corp-state-links">"""
+        u_key = "state"
+        if len(lwargs) == 1:
+            u_key = "city"
+        loc_objs = list(coll_ra.find(lwargs, {'_id': 0}).distinct(u_key))
+        for i in loc_objs:
+            rel_link = urllib.parse.quote(f"/agents-by-state/{'/'.join([*lwargs.values()])}/{i}/")
+            if dbg is True:
+                rel_link = urllib.parse.quote(f"/agents/compile/{site.id}/agents-by-state/{'/'.join([*lwargs.values()])}/{i}/")
+            location_table += f"""<a href="{rel_link}">{i}</a>"""
+        location_table += "</div>"
+    
+    rep_codes = {
+        'XXsitemetasXX': f"{site.sitemetas}",
+        'XXpagemetasXX': f"{pagedoc.pagemetas}",
+        'XXsitelinksXX': f"{site.sitelinks}",
+        'XXpagelinksXX': f"{pagedoc.pagelinks}",
+        'XXsitestyleXX': f"{site.sitestyle}",
+        'XXtitleXX': f"{pagedoc.title}",
+        'XXsiteheaderXX': f"{site.siteheader}",
+        'XXcontentXX': f"{pagedoc.content}",
+        'XXsitefooterXX': f"{site.sitefooter}",
+        'XXsitescriptsXX': f"{site.sitescripts}",
+        'XXpagescriptsXX': f"{pagedoc.pagescripts}",
+        
+    }
+    compiled = f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        XXsitemetasXX
+        XXpagemetasXX
+        XXsitelinksXX
+        XXpagelinksXX
+        XXsitestyleXX
+        <title>XXtitleXX</title>
+    </head>
+    <body>
+        XXsiteheaderXX
+        XXcontentXX
+        XXsublocationsXX
+        XXsitefooterXX
+        XXsitescriptsXX
+        XXpagescriptsXX
+    </body>
+    </html>"""
+
+    for k, v in rep_codes.items():
+        compiled = re.sub(k, v, compiled)
+
+    compiled = re.sub("XXagentsXX", agent_table, compiled)
+
+    compiled = re.sub("XXsublocationsXX", agent_table, compiled)
+
     return HttpResponse(compiled, content_type='text/plain')
 
 def agents_by_corp(request, site, pagename, **kwargs):
