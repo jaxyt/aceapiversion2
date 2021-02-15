64271db105beb69e26035495f1d5449710c87df2
jaxyt <jax.maximillion.taylor@gmail.com>
2021-02-15 16:48:12 -0500

    b

--- agents/urls.py
+++ agents/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import agent_create_and_list_view, compilerv4, compilerv5, get_home, upload_agents, AgentDeleteView, AgentUpdateView, abs_main, abs_state, abs_city, ps_agent, ps_state, ps_city, ra_search, ra_agent
+from .views import agent_create_and_list_view, compilerv4, compilerv5, get_home, upload_agents, AgentDeleteView, AgentUpdateView, home_view, abs_main, abs_state, abs_city, ps_agent, ps_state, ps_city, ra_search, ra_agent
 
 app_name = 'agents'
 
 urlpatterns = [
     path('', agent_create_and_list_view, name='main-agent-view'),
     #path('upload/', upload_agents, name='upload-agent-view'),
-
+    path('test/<int:siteid>/', home_view),
     path('test/<int:siteid>/agents-by-state/', abs_main),
     path('test/<int:siteid>/agents-by-state/<str:state>/', abs_state),
     path('test/<int:siteid>/agents-by-state/<str:state>/<str:city>/', abs_city),
