cfbf9fd017467fdb838cbd3e1ccef75f3393f984
jaxyt <jax.maximillion.taylor@gmail.com>
2021-02-15 15:58:23 -0500

    b

--- agents/views.py
+++ agents/views.py
@@ -575,7 +575,6 @@ def ps_city(request, *args, **kwargs):
             for k, v in rep_codes.items():
                 res = re.sub(k, v, res)
             res = re.sub('XXagentsXX', agent_table, res)
-            res = re.sub('XXsublocationsXX', location_table, res)
             for k, v in rep_args.items():
                 res = re.sub(k, v, res)
             res = replace_shortcodes(site, res)
